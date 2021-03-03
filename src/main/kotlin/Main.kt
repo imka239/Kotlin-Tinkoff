6159eef90a078ff38f18201d2c34886de79d6287
imka239 <ima2ima3go9@gmail.com>
2021-03-03 15:37:55 +0300

    enqueue and dequeue added as functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,7 +4,7 @@ fun main() {
 
     for (i in 0 until 10) {
         stack.push(i)
-        queue.push(i)
+        queue.enqueue(i)
     }
     println("Check stack pop")
     for (i in 0 until 11) {
@@ -12,7 +12,7 @@ fun main() {
     }
     println("Now check queue pop")
     for (i in 0 until 11) {
-        println(queue.pop())
+        println(queue.dequeue())
     }
     stack.push(4)
     stack.push(4)
--- src/main/kotlin/Queue.kt
+++ src/main/kotlin/Queue.kt
@@ -4,11 +4,11 @@ import java.util.*
 class Queue<E> {
     private val queue = LinkedList<E>()
 
-    fun push(elem : E) {
+    fun enqueue(elem : E) {
         queue.add(elem)
     }
 
-    fun pop(): E? {
+    fun dequeue(): E? {
         if (queue.isEmpty()) {
             println("queue is empty")
             return null
